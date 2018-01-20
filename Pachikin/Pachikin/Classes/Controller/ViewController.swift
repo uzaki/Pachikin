a206ca7469f2cf5d7aca20858ce074f2072b27c8
uzaki kota <uzaki@planningdevnoMacBook-Air.local>
2018-01-20 10:54:12 +0900

    setting directory

---
+++ Pachikin/Pachikin/Classes/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pachikin
+//
+//  Created by uzaki kota on 2018/01/20.
+//  Copyright © 2018年 uzaki kota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
